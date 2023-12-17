315b934cd31249e6213dda656d2acbc9eec4fd5c
Shoaib <shoaib@gmail.com>
2023-12-17 23:12:25 +0430

    updated Q14

--- Answer14.js
+++ Answer14.js
@@ -1,7 +1,7 @@
 function isEqual(obj1, obj2) {
     return (obj1) === (obj2);
   }
-  var obj1 = { name: "John", age: 30 };
+  var obj1 = { name: "JohnWick", age: 30 };
   var obj2 = { name: "Jane", age: 25 };
   
   console.log(isEqual(obj1, obj2)); 
